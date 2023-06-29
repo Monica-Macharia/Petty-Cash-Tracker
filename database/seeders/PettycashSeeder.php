5f62220334613be21587162cd66e2b7f5c10f6db
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-06-29 23:57:55 +0300

    seeding data

--- database/seeders/PettycashSeeder.php
+++ database/seeders/PettycashSeeder.php
@@ -13,6 +13,38 @@ class PettycashSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $pettycashes = [
+            [
+             'name' => 'Dominic Mwanza',
+             'payment_details' => '0078009900',
+             'service' => 'Products transportation',
+             'cash_payable' => 600,
+             'status' => 'Not Paid',
+            ],
+            [
+                'name' => 'Angela Mwanza',
+                'payment_details' => '0078009901',
+                'service' => 'Packaging materials',
+                'cash_payable' => 6000,
+                'status' => 'Not Paid',
+            ],
+            [
+                'name' => 'Alize Mwanzako',
+                'payment_details' => '0078909900',
+                'service' => 'Products packaging',
+                'cash_payable' => 800,
+                'status' => 'Not Paid',
+            ],
+            [
+                'name' => 'Elisha Mwata',
+                'payment_details' => '0978089900',
+                'service' => 'Book purchase',
+                'cash_payable' => 600,
+                'status' => 'Paid',
+               ]
+         ];
+        
+        
+         DB::table('pettycashes')->insert($pettycashes);
     }
 }
