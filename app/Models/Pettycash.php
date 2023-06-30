6971a42e8eed1360c947c343b245b861eb2117d8
Monica Nyaguthii Macharia <monicamacharia7@gmail.com>
2023-06-30 07:48:42 +0300

    fillable setting

--- app/Models/Pettycash.php
+++ app/Models/Pettycash.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Pettycash extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'payment_details',
+        'service',
+        'cash_payable',
+        'status'
+    ];
 }
